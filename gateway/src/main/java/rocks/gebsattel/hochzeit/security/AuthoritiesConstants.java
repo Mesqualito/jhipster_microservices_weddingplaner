8be779be00580e2fb11af1524d8476e51c089d6d
Jochen Hassfurter <jo@hassfurter.com>
2018-05-20 12:49:12 +0200

    TODO: for individual Per-User-ROLES see: - https://stackoverflow.com/questions/50416007/how-to-create-a-new-authority-in-jhipster/50433463#50433463 - https://stackoverflow.com/questions/50085892/jhipster-decode-jwt-token-access-to-roles - https://stackoverflow.com/questions/50416007/how-to-create-a-new-authority-in-jhipster - https://stackoverflow.com/questions/50434004/jhipster-individual-user-roles-for-personal-data-and-post-authorisation

--- gateway/src/main/java/rocks/gebsattel/hochzeit/security/AuthoritiesConstants.java
+++ gateway/src/main/java/rocks/gebsattel/hochzeit/security/AuthoritiesConstants.java
@@ -1,5 +1,12 @@
 package rocks.gebsattel.hochzeit.security;
 
+
+// for individual Per-User-ROLES see:
+// - https://stackoverflow.com/questions/50416007/how-to-create-a-new-authority-in-jhipster/50433463#50433463
+// - https://stackoverflow.com/questions/50085892/jhipster-decode-jwt-token-access-to-roles
+// - https://stackoverflow.com/questions/50416007/how-to-create-a-new-authority-in-jhipster
+// - https://stackoverflow.com/questions/50434004/jhipster-individual-user-roles-for-personal-data-and-post-authorisation
+
 /**
  * Constants for Spring Security authorities.
  */
